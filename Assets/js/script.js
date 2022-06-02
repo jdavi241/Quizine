f12bad393d44198e1659b8a29be1a93054a78a26
jdavi241 <davisjimm25@gmail.com>
2022-06-02 18:38:04 -0500

    indentation, hid questions, organized code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 var startQuiz = document.querySelector("#startBtn");
 startQuiz.addEventListener("click", getQuestion);
-
+var questionCon = document.querySelector("#questionDsp");
 var startMenu = document.querySelector(".startMenu");
 
 
@@ -14,38 +14,50 @@ var question = [{
 },
 {
   question: "Tacos or Soup?",
-  answers: ["Tacos", "Soup"]}]
+  answers: ["Tacos", "Soup"]
+},
+{
+  question: "Chicken or Salad",
+  answers: ["Chicken", "Salad"]
+}]
+
 var i=0
 function getQuestion() {
   var questionEl = document.querySelector(".que_text");
   var answerOneEl = document.querySelector("#one");
   var answerTwoEl = document.querySelector("#two");
-  answerOneEl.addEventListener("click", answerClick)
-  answerTwoEl.addEventListener("click", answerClick)
-  
-startMenu.style.display = "none"
+  answerOneEl.addEventListener("click", answerClick);
+  answerTwoEl.addEventListener("click", answerClick);
 
-questionEl.innerText = question[i].question
-answerOneEl.innerText = question[i].answers[0]
-answerTwoEl.innerText = question[i].answers[1]
+  // Hide start menu
+  startMenu.style.display = "none";
+  // Display questions
+  questionCon.style.display = "block";
 
+  // Go to question array and insert "question:"
+  questionEl.innerText = question[i].question;
 
-  // if (answers = "out") {
-   // console.log(getQuestion);
-  //}
-
-  //else (answers = "in")
-
-
+  answerOneEl.innerText = question[i].answers[0];
+  answerTwoEl.innerText = question[i].answers[1];
 };
+
+// Hide question content on home page
+questionCon.style.display = "none";
 
 var answerOpt = []
 
 function answerClick() {
-  var questionCon = document.querySelector("#questionDsp");
+  //var questionCon = document.querySelector("#questionDsp");
+
+  // insert html into empty array 
   answerOpt.push(this.innerText)
+
+  // iterate through each question on click 
   i++
+
+  // Grab next question
   getQuestion();
+
   console.log(answerOpt);
   //console.log(this.innerText);
 }
